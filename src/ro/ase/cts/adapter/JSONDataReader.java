1372b8c44b40587df953210a51f3abbc13af2e75
Dobre Aurel-Ninel <101126905+AurelDobre20@users.noreply.github.com>
2024-04-09 23:55:49 +0300

    Initial commit

---
+++ src/ro/ase/cts/adapter/JSONDataReader.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.adapter;
+
+public class JSONDataReader {
+    String readJSON(){
+        return "Data from JSON";
+    }
+
+}
---
+++ src/ro/ase/cts/adapter/Main.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        DataReader xmlAdapter = new XMLAdapter(new XMLDataReader());
+        DataReader jsonAdapter = new JSONAdapter(new JSONDataReader());
+        System.out.println(xmlAdapter.readData());
+        System.out.println(jsonAdapter.readData());
+    }
+}
